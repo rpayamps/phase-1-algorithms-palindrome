429e70b8b9659c624fbe730e113a070f1c175b6a
root <root@DESKTOP-UHS28DU.localdomain>
2022-05-17 21:08:28 -0400

    commit tree d6587ed686d240f49d12ccade68367a83297d018

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // Write your algorithm here  
+    if ( word.split('').reverse().join('') === word) {
+         return true
+      }
+    else {
+        return false
+      }
+}   
 
 /* 
   Add your pseudocode here
+  initalize an array called result
+
+  iteraite over the string and return any string that has the same character in the index position of 0 and the index end of that string 
+
+  return result
 */
 
 /*
   Add written explanation of your solution here
+  I am looking to see if a string if that strings characters read in both directions are the same
 */
 
 // You can run `node index.js` to view these console logs
